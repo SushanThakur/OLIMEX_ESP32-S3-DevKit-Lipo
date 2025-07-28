470febca6ab72e74588932b1755e63916280e647
sushanthakur <mail@sushant.uk>
2025-07-28 22:53:12 +0545

    add asynchronous code for homing

--- armikochan_firmware/armikochan_firmware.ino
+++ armikochan_firmware/armikochan_firmware.ino
@@ -1,12 +1,30 @@
 #include <AccelStepper.h>
 #include <MultiStepper.h>
+#include <Wire.h>
 
+// ESP CONFIG
+#define BAUD 115200
+#define I2C_SDA 15
+#define I2C_SCL 16
+
+// TIME CONST
+#define DEBOUNCE_DELAY_HOME 100 // In Microseconds
+#define REVERSE_INTERVAL 200 // In Miliseconds
+
+// PHYSICAL CONST
 #define STEPPER_NUM 7
-#define DEBOUNCE_DELAY 100 // In Microseconds
+
+// STEPPER PARAM
+#define STEPPER_MAX_SPEED 2000
+#define STEPPER_RUN_SPEED 1000
+#define STEPPER_ACEL 5000
 
 const int dir_pins[STEPPER_NUM] = { 4,5,6,7,17,18,8 };
 const int step_pins[STEPPER_NUM] = { 3,9,10,11,12,13,14 };
-const int hall_pins[STEPPER_NUM] = { 35,45,48,47,21, };
+const int hall_pins[STEPPER_NUM] = { 21,47,48,45,35,39,40 };
+
+bool stepper_homed[STEPPER_NUM] = {false};
+bool all_homed = false;
 
 AccelStepper stepper[STEPPER_NUM] = {
   AccelStepper(AccelStepper::DRIVER, step_pins[0], dir_pins[0]),
@@ -20,54 +38,73 @@ AccelStepper stepper[STEPPER_NUM] = {
 
 MultiStepper multi;
 
-void home_steppers(){
-  void home_steppers() {
-  Serial.println("DEBUG: Starting Homing...");
+void TCA9548A(uint8_t bus){
+  Wire.beginTransmission(0x70);  // TCA9548A address
+  Wire.write(1 << bus);          // send byte to select bus
+  Wire.endTransmission();
+  Serial.print(bus);
+}
 
-  for(int i=0; i<STEPPER_NUM; i++) {
-    
-    // First Homing Cycle
-    while (true) {
-      stepper[i].runSpeed();
-
+void steppers_move_n_check() {
+  while (!all_homed) {
+    for (int i=0; i<STEPPER_NUM; i++){
+      if (!stepper_homed[i]){
+        stepper[i].runSpeed();
+      }
       if(digitalRead(hall_pins[i]) == HIGH) {
-        delayMicroseconds(DEBOUNCE_DELAY);
+        delayMicroseconds(DEBOUNCE_DELAY_HOME);
+        Serial.println("DEBUG: Switch Triggered Frist Time");
         if(digitalRead(hall_pins[i]) == HIGH) {
-          Serial.println("DEBUG: Switch Triggered");
-          break;
+          Serial.println("DEBUG: Switch Triggered Second Time");
+          stepper_homed[i] = true;
         }
       }
     }
-
-    // Move in opposite direction for second homing cycle
-    stepper[i].setSpeed(-1000); //Reverse Direction
-    unsigned long start_time = millis();
-    unsigned long interval = 200;
-    while(millis() - start_time < interval) {
-      stepper[i].runSpeed();
-    }
-
-    // Second Homing Cycle
-    while (true) {
-      stepper[i].runSpeed();
-
-      if(digitalRead(hall_pins[i]) == HIGH) {
-        delayMicroseconds(DEBOUNCE_DELAY);
-        if(digitalRead(hall_pins[i]) == HIGH) {
-          Serial.println("DEBUG: Switch Triggered");
-          break;
-        }
+    all_homed = true;
+    for (int i=0; i<STEPPER_NUM; i++) {
+      if (!stepper_homed[i]) {
+        all_homed = false;
       }
     }
-
-    stepper[i].setCurrentPosition(0);
-    stepper[i].setAcceleration(5000);
   }
 }
-};
-void update_positions();
-void update_angles();
-void set_gripper();
+
+void home_steppers(){
+  Serial.println("DEBUG: Starting Homing");
+
+  /*
+    
+  */
+
+  steppers_move_n_check();
+
+  // Reset stepper_homed variable
+  for (int i=0; i<STEPPER_NUM; i++) {
+    stepper_homed[i] = false;
+    stepper[i].setSpeed(-STEPPER_RUN_SPEED); // to make the stepper rotate in opposite direction
+  }
+
+  // Move in opposite direction for 'reverse_interval' time period
+  unsigned long reverse_start_time = millis();
+  while(millis() - reverse_start_time < REVERSE_INTERVAL){
+    for(int i=0; i<STEPPER_NUM; i++){
+      stepper[i].runSpeed();
+    }
+  }
+
+  steppers_move_n_check();
+
+  for(int i=0; i<STEPPER_NUM; i++){
+    stepper[i].setCurrentPosition(0);
+    stepper[i].setAcceleration(STEPPER_ACEL);
+  }
+
+  Serial.println("DEBUG: Homing Done");
+}
+
+void update_positions(){};
+void update_angles(){};
+void set_gripper(){};
 
 void setup(){
   Serial.begin(115200);
@@ -76,12 +113,12 @@ void setup(){
     pinMode(dir_pins[i], OUTPUT);
     pinMode(step_pins[i], OUTPUT);
     pinMode(hall_pins[i], INPUT);
-    stepper[i].setMaxSpeed(2000);
-    stepper[i].setSpeed(1500);
+    stepper[i].setMaxSpeed(STEPPER_MAX_SPEED);
+    stepper[i].setSpeed(STEPPER_RUN_SPEED);
     multi.addStepper(stepper[i]);
   }
 
-  Serial.println("INITIALIZED");
+  Serial.println("DEBUG: Setup Complete");
 }
 
 void loop(){
