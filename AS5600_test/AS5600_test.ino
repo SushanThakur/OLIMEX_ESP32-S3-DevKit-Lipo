3fb716be87558dff5103dc34f55916059fdb0fbb
Sushant Thakur <mail@sushant.uk>
2025-08-05 21:30:58 +0545

    minor updates

--- AS5600_test/AS5600_test.ino
+++ AS5600_test/AS5600_test.ino
@@ -16,7 +16,7 @@ void setup()
   Serial.println();
 
   Wire.begin(21,22);
-  Wire.setClock(50000);
+  Wire.setClock(50000); // can be set as low as 1k but is very slow 
 
   as5600.begin(4);  //  set direction pin.
   as5600.setDirection(AS5600_CLOCK_WISE);  //  default, just be explicit.
